0e30efba617669f95c2abc9cc6f0d8e4aa63a76c
azhar <azharahmedtakoy4444@gmail.com>
2024-06-03 11:17:46 +0300

    💄 updated  the navbar component

---
+++ JS/navbar.js
@@ -0,0 +1,16 @@
+class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.loadNavbar();
+    }
+
+    loadNavbar() {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                this.innerHTML = data;
+            })
+            .catch(error => console.error('Error fetching navbar:', error));
+    }
+}
+
+customElements.define('custom-navbar', Navbar);
