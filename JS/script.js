9a1c6d082b4847b3d44f738a2af2e92f47817413
KristinaKemoi <kristina.kemoi@strathmore.edu>
2024-06-07 20:43:27 +0300

    Home page with navigation bar

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const contentText = document.querySelector(".content-text");
     const readMore = document.getElementById("read-more");
+    const menuBar = document.querySelector('.menuBar');
+    const nav_links = document.querySelector('.nav_links');
 
     // Check if the content text is overflowing
     if (contentText.scrollHeight > contentText.clientHeight) {
@@ -11,4 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
         contentText.style.maxHeight = "none";
         readMore.style.display = "none";
     });
+
+    menuBar.addEventListener('click', () => {
+        nav_links.classList.toggle('active');
+    });
+
+    searchButton.addEventListener('click', performSearch);
+
 });
+function performSearch() {
+    const query = document.getElementById('search').value;
+    if (query) {
+        alert(`Searching for: ${query}`);
+    } else {
+        alert('Please enter a search term.');
+    }
+}
