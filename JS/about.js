a8897e67d14bd3a87d77d830be00008457ac14a8
NdanuKyalo <laureen.kyalo@strathmore.edu>
2024-06-13 15:51:43 +0300

    Finished the about us page

---
+++ JS/about.js
@@ -0,0 +1,29 @@
+class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.loadNavbar();
+    }
+
+    loadNavbar() {
+        fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                this.innerHTML = data;
+            })
+            .catch(error => console.error('Error fetching navbar:', error));
+    }
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        const menuBar = document.getElementById('menuBar');
+        const menu = document.getElementById('menu');
+    
+        menuBar.addEventListener('click', () => {
+            console.log("clicked")
+            menu.classList.toggle('active');
+        });
+    });
+    
+customElements.define('custom-navbar', Navbar);
+
+var video = document.querySelector("video");
+video.muted = true;
+video.play();
